43ec1e1341a1761733bdcb2f7f9d6fa475067fec
NiltonFG <nilton.felipe.gavazzi@gmail.com>
2021-03-19 23:09:05 -0300

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express = require('express')
+var mysql = require('mysql')
 const app = express()
 const port = 3000
+var morgan = require('morgan')
+app.use(morgan('combined'))
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database:"db_app"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Olá mundo!')
 })
 
-app.get('/encanado2', (req, res) => {
-    res.send('Hello bixa')
+app.get('/encanado', (req, res) => {
+    res.send()
+  })
+
+app.use('/login', (req, res) => {
+    console.log(req.body.usuario);
+    nome = req.body.usuario;
+    senha = req.body.senha;
+    con.connect(function(err){
+      if (err) throw err;
+    con.query('SELECT usuario,senha FROM usuarios where usuario = ${nome}  and senha = ${senha}'
+    , function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+    });
+    res.send()
   })
 
 app.listen(port, () => {
